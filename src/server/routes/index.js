38d3f238b831f5dc55c09080e6055ccd85463249
Ryan Greenberg <hawk1138@gmail.com>
2017-08-30 16:58:38 -0700

    major overhaul step 1

---
+++ src/server/routes/index.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const request = require('request-json');
+let client = request.createClient('https://api.github.com/')
+
+router.get('/', (req, res) => {
+  res.render('home')
+})
+
+router.get('/projects', (req, res) => {
+
+  client.get('users/optomal7/events/public')
+    .then(function(result) {
+      for (var i = 0; i < result.body.length; i++) {
+        if (result.body[i].type === 'PushEvent') {
+          return {repoName: result.body[i].repo.name,
+                  url: "https://github.com/" + result.body[i].repo.name + "/commit/" + result.body[i].payload.head,
+                  time: result.body[i].created_at,
+                  message: result.body[i].payload.commits[0].message }
+        }
+      }
+    })
+    .then((mostRecent) => {
+      res.render('projects', {mostRecent})
+  })
+})
+
+router.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
+module.exports = router
